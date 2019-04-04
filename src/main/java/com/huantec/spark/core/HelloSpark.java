1d64281ba8eac3eafa33bb74d170dacf7e46681a
liuhuanjs <liuhuanjs@163.com>
2019-04-04 21:37:45 +0800

    Spark branch init.

---
+++ src/main/java/com/huantec/spark/core/HelloSpark.java
@@ -0,0 +1,10 @@
+package com.huantec.spark.core;
+
+/**
+ * @author liuhuan 2019-04-04
+ */
+public class HelloSpark {
+    public static void main(String[] args) {
+        System.out.println("hello spark");
+    }
+}
